5f375cf8cb1bdd0e0e947627e5e6cfba31324963
Ashim Debnath <ashimdebnath6767@gmail.com>
2025-11-10 23:23:25 +0530

    set up done

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
+// server.js or index.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello from Express!");
+
+app.use(cors());
+
+app.get('/home', (req, res) => {
+    res.status(200).json({ status: 'OK', data: "Hello FOSS Club" })
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
