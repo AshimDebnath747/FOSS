85dfc6f1878f535c99b9a19649fb56cd4bc54604
Ashim Debnath <ashimdebnath6767@gmail.com>
2025-12-07 09:05:31 +0530

    oauth added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,10 +2,31 @@ import express from 'express';
 import { validate } from '../middlewares/validate.middleware.js';
 import { loginSchema, signupSchema } from '../validators/auth.validator.js';
 import { login, signup } from '../controllers/auth.controller.js';
+import passport from 'passport';
 const router = express.Router()
 
 router.post("/signup", validate(signupSchema), signup);
 router.post("/login", validate(loginSchema), login);
+router.get(
+    "/google-auth",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", { session: false }),
+    (req, res) => {
+        const token = req.user.token;
+
+        res.cookie("token", token, {
+            httpOnly: true,     // ✅ JS cannot access it
+            secure: false,      // ✅ true in production with HTTPS
+            sameSite: "lax",    // ✅ prevents CSRF
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        });
+        res.redirect(`http://localhost:5173/`);
+    }
+);
 //router.post("/google-auth");
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/auth.routes.js'
+import cookieParser from "cookie-parser";
+import passport from 'passport';
+import "./config/passport.js"
 const app = express();
 
 app.use(express.json())
 app.use(cors());
+app.use(cookieParser());
+app.use(passport.initialize());
 
 app.use('/api/auth', authRouter);
 
